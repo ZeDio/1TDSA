86eea86a1f0cbbb92fc563015af27adc15513101
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-05 11:52:34 -0300

    aula front

---
+++ Front-end Design Engineering/Aula 10 - 05.05/js/script.js
@@ -0,0 +1,15 @@
+// alert('hello word')
+
+// var nome='José'
+// var nomeDigitado = prompt("Digite seu nome o infeliz")
+// alert("Olá "+nomeDigitado+", seja vem vindo!")
+
+// n1 = parseInt(prompt("Digite o numero o caraio"))
+// n2 = parseInt(prompt("Digite o numero o caraio"))
+// alert(n1+n2)
+
+// var n1, n2, result;
+
+function ola(nome){
+    alert("Hellow Word ola")
+}
