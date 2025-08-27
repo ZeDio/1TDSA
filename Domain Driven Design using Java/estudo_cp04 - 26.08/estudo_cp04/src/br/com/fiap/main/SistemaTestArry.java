62477bc1e8228cef3ade4f0736ccc73d10d4a7a2
ZeDio <jose.diogo100407@gmail.com>
2025-08-26 23:02:31 -0300

    estudo cp04 java, parte de arrylist nn ta pegando

--- Domain Driven Design using Java/estudo_cp04 - 26.08/estudo_cp04/src/br/com/fiap/main/SistemaTestArry.java
+++ Domain Driven Design using Java/estudo_cp04 - 26.08/estudo_cp04/src/br/com/fiap/main/SistemaTestArry.java
@@ -3,40 +3,42 @@ package br.com.fiap.main;
 import br.com.fiap.beans.Produto;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class SistemaTestArry {
 
     static String texto(String t){
       return JOptionPane.showInputDialog(t);
     };
-    static int number(String t){
+    static int texto2(String t){
         return Integer.parseInt(JOptionPane.showInputDialog(t));
     };
-    static double number2(String t){
+    static double texto3(String t){
         return Double.parseDouble(JOptionPane.showInputDialog(t));
     };
 
     public static void main(String[] args) {
-        Produto[] vetorProduto = new Produto[3];
-        int i = 0;
-        do{
-            vetorProduto[i]=new Produto();
-            vetorProduto[i].setNome(texto("Nome Produto"));
-            vetorProduto[i].setEstoque(number("Quantidade Estoque"));
-            vetorProduto[i].setPreco(number2("Valor"));
-            i++;
-        }while(JOptionPane.showConfirmDialog(null,
-                "cadastrar produto",
-                "cadastrar produto",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE) == 0);
+        ArrayList<Produto> listaProduto = new ArrayList<Produto>();
 
-        for(int ii = 0; ii < i; ii++){
+        Produto objProduto = null;
+
+        do {
+            objProduto.setNome(texto("Nome Produto"));
+            objProduto.setEstoque(texto2("Estoque"));
+            objProduto.setPreco(texto3("Preço"));
+            listaProduto.add(objProduto);
+        }while (JOptionPane.showConfirmDialog(null,
+                "Cadastrar Produto",
+                "Cadastrar Produto",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE)==0);
+
+        for (Produto a: listaProduto){
             System.out.println(
-                    "nome"+vetorProduto[ii].getNome()+
-                            "\nQuantidade Estoque"+vetorProduto[ii].getEstoque()+
-                            "\nValor"+vetorProduto[ii].getPreco()
+                    "Nome"+a.getNome()+
+                            "Estoque"+a.getEstoque()+
+                            "Valor"+a.getPreco()
             );
-        };
+        }
     }
 }
--- Domain Driven Design using Java/estudo_cp04 - 26.08/estudo_cp04/src/br/com/fiap/main/SistemaTestVetor.java
+++ Domain Driven Design using Java/estudo_cp04 - 26.08/estudo_cp04/src/br/com/fiap/main/SistemaTestVetor.java
@@ -1,20 +1,40 @@
 package br.com.fiap.main;
 
+import br.com.fiap.beans.Produto;
+
 import javax.swing.*;
 
 public class SistemaTestVetor {
 
-    static String texto(String t){
-        return JOptionPane.showInputDialog(t);
+    static String texto(String b){
+       return JOptionPane.showInputDialog(b);
     };
-    static int number(String t){
-        return Integer.parseInt(JOptionPane.showInputDialog(t));
+    static int texto2(String b){
+        return Integer.parseInt(JOptionPane.showInputDialog(b));
     };
-    static double number2(String t){
-        return Double.parseDouble(JOptionPane.showInputDialog(t));
+    static double texto3(String b){
+        return Double.parseDouble(JOptionPane.showInputDialog(b));
     };
 
     public static void main(String[] args) {
+        Produto[] vetorProduto = new Produto[3];
+        int i = 0;
+        do {
+            vetorProduto[i] = new Produto();
+            vetorProduto[i].setNome(texto("Nome Produto"));
+            vetorProduto[i].setEstoque(texto2("Estoque"));
+            vetorProduto[i].setPreco(texto3("Valor"));
+            i++;
+        }while(JOptionPane.showConfirmDialog(null,"Cadrastrar Produto",
+                "Cadastrar Produto", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE)==0);
 
+        for (int ii = 0; ii < i; ii++){
+            System.out.println(
+                    "Nome"+vetorProduto[ii].getNome()+
+                            "Estoque"+vetorProduto[ii].getEstoque()+
+                            "Valor"+vetorProduto[ii].getPreco()
+            );
+        };
     }
 }
